4d56a2cede911ce477bb21c006a849f6bc085d06
Alexandra Krah <12krah@cloak.mafiasi.de>
2016-04-29 14:40:19 +0200

    Sender Class

--- CycleCity_A2/app/src/main/java/digitale_stadt/cyclecity_a2/Sender.java
+++ CycleCity_A2/app/src/main/java/digitale_stadt/cyclecity_a2/Sender.java
@@ -1,14 +1,33 @@
 package digitale_stadt.cyclecity_a2;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 /**
  * Created by alexutza_a on 29.04.2016.
  */
 public class Sender {
-    static Position position;
+     static Position position;
 
     public static String POST (Position pos){
 
         position = pos;
+
+        String json = "";
+
+        //Build JSON object
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.accumulate("Timestamp", position.getTime());
+            jsonObject.accumulate("Latitude", position.getLatitude());
+            jsonObject.accumulate("Longitude", position.getLongitude());
+            jsonObject.accumulate("Altitude", position.getAltitude());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        //Convert jsonObject to String
+        json = jsonObject.toString();
 
         return "" + position;
     }
